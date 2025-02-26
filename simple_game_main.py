508a10ce92b8bf8871b78ebe1de6f418e5af5e39
scarggy-design1 <lizzy.saldana@ucas-edu.net>
2025-02-26 13:14:48 -0700

    yes

---
+++ simple_game_main.py
@@ -0,0 +1,21 @@
+#This is Nicole's simple game user interface
+from simple_games import one_ten as ten, one_hundred as hundred
+
+
+def simple_game_main():
+    while True:
+        choice = input("\nWelcome to the SIMPLE GUESSING GAME!\n What would you like to do? \n 1) Guess 1-10\n 2) Guess 1-100\n 3) EXIT to the main menu\n")
+        if choice == '1':
+            while True:
+                oneTen = ten()
+                if 'end' in oneTen:
+                    break  
+        elif choice == '2':
+            while True:
+                oneHundred = hundred()
+                if 'end' in oneHundred:
+                    break
+        elif choice == '3':
+            break
+
+simple_game_main()
---
+++ simple_games.py
@@ -0,0 +1,93 @@
+#This is Nicole's simple game functions
+import random
+
+def one_ten():
+    score = 0
+    current = 0
+    streak = 0
+    while True:
+            comp_num = random.randint(1,10)
+            while True:
+                try:
+                    user_num = int(input("\nGuess a number from 1-10!\n      "))
+                    if user_num >= 1 and user_num <= 10:
+                        pass
+                    else:
+                        print("_______\nCHOOSE FROM 1-10\n_______")
+                        continue
+                except ValueError:
+                    print("Please choose a valid NUMBER.")
+                    continue
+                if user_num == comp_num:
+                    print("\n--------------CONGRATTS!!!! YOU DID IT----------------\n")
+                    streak+=1
+                    if current <= 3 and current > 0:
+                        score = score+streak*2
+                        current = 0
+                    elif current > 3:
+                        score+=1
+                        current = 0
+                    elif current == 0:
+                        score = score+streak*4
+                    while True:
+                        play = input(f"SCORE: {score}\nSTREAK: {streak}\nWould you like to keep playing?(yes or no)\n ").title()
+                        if play == 'No':
+                            return score, 'end'
+                        elif play == 'Yes':
+                            break
+                        elif play != 'Yes' and play != 'No':
+                            print("Choose yes or no. ")
+                            continue
+                    break
+                elif user_num != comp_num:
+                    print("Nope! Try again!")
+                    print("\n")
+                    streak = 0
+                    current+=1
+                    continue
+
+
+def one_hundred():
+    score = 0
+    current = 0
+    streak = 0
+    while True:
+            comp_num = random.randint(1,100)
+            while True:
+                try:
+                    user_num = int(input("\nGuess a number from 1-100!\n      "))
+                    if user_num >= 1 and user_num <= 100:
+                        pass
+                    else:
+                        print("-------\nCHOOSE FROM 1-100\n-------")
+                        continue
+                except ValueError:
+                    print("Please choose a valid NUMBER.")
+                    continue
+                if user_num == comp_num:
+                    print("\n--------------CONGRATTS!!!! YOU DID IT----------------\n")
+                    streak+=1
+                    if current <= 3 and current > 0:
+                        score = score+streak*2
+                        current = 0
+                    elif current > 3:
+                        score+=1
+                        current = 0
+                    elif current == 0:
+                        score = score+streak*4
+                    while True:
+                        play = input(f"SCORE: {score}\nSTREAK: {streak}\nWould you like to keep playing?(yes or no)\n ").title()
+                        if play == 'No':
+                            return score, 'end'
+                        elif play == 'Yes':
+                            break
+                        elif play != 'Yes' and play != 'No':
+                            print("Choose yes or no. ")
+                            continue
+                    break
+                elif user_num != comp_num:
+                    print("Nope! Try again!")
+                    print("\n")
+                    streak = 0
+                    current+=1
+                    continue
