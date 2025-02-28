8762b41a9d227f1c5ad48ef233577580ff54f8a7
UCAS-Sawyer <sawyer.wood@ucas-edu.net>
2025-02-27 17:57:53 -0700

    removed useless elif statements(removed complex game options)

--- High Score Tracker/main.py
+++ High Score Tracker/main.py
@@ -18,8 +18,7 @@ def main(): #Cecily did this
                         choose_board=int(input("""What do you want to do?
                                             1. 1-10 scoreboard
                                             2. 1-100 scoreboard
-                                            3. complex scoreboard
-                                            4. exit\n"""))
+                                            3. exit\n"""))
                         if choose_board==1:
                             score_printer("High Score Tracker/simple_game1-10.csv")
                             break
@@ -27,9 +26,6 @@ def main(): #Cecily did this
                             score_printer("High Score Tracker/simple_game1-100.csv")
                             break
                         elif choose_board==3:
-                            print(complex_scores())
-                            break
-                        elif choose_board==4:
                             break
                         else:
                             print('invalid option')
@@ -41,15 +37,11 @@ def main(): #Cecily did this
                     try:
                         choose_board=int(input("""What do you want to play?
                                             1. simple game
-                                            2. complex game
-                                            3. exit\n"""))
+                                            2. exit\n"""))
                         if choose_board==1:
                             simple_game()
                             break
                         elif choose_board==2:
-                            complex_game()
-                            break
-                        elif choose_board==3:
                             break
                         else:
                             print('invalid option')
